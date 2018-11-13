830a6d8ae94a4a3d6da54d704577def79fafb699
vinbug1 <vincentwilliamsjohn9626@gmail.com>
2018-11-14 00:28:24 +0100

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+
+            public static string ReverseString(string s)
+            {
+                string tmp = string.Empty;
+
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    tmp += s[i].ToString();
+                }
+
+                return tmp;
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine(ReverseString("Vincent"));
+                Console.Read();
+            }
+        
+    }
+   
+}
